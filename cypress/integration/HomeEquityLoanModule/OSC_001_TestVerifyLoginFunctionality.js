393d274ab23928effdfc6867a01e43cef5bd60c9
Priy <priya.gandhi@infostride.com>
2022-12-19 10:19:31 -0500

    Commit

--- cypress/integration/HomeEquityLoanModule/OSC_001_TestVerifyLoginFunctionality.js
+++
@@ -1,9 +0,0 @@
-/// <reference types="cypress" />
-
-describe('Login into Oscar', function () {
-
-    it('Login Into Oscar Website', function () {
-        cy.Login()
-        cy.Logout()
-    })
-})
